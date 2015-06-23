a20d462acaba515cd0b917d6e2f16c15f17e3adb
sfranke <sfr4nke@gmail.com>
2015-06-23 16:41:47 +0200

    Added logic to support unique API-keys.

--- ts3bot.js
+++ ts3bot.js
@@ -52,6 +52,8 @@ function date() {
 		if (response.msg.length == 72) {
 			console.log(date() + " [Info] checking valid key");
 			console.log(date() + " [Info] " + "'" + response.msg + "'");
+			serverQueryClient.send("sendtextmessage", {targetmode: "1", target: userId, msg: 'Your key is now getting validated by AarenaNet, please wait a moment.'}, function (err, response){
+			});
 
 			var token = response.msg;
 			var options = {
@@ -66,75 +68,95 @@ function date() {
 				console.log(date() + " [Info] gw2 API status code: ", res.statusCode);
 				//console.log("headers: ", res.headers);
 
-				//if (res.statusCode === 400) {
-				//	console.log('[HTTP_error_code]: ' + res.statusCode + ' server not responding!');
-				//	//Add logic to handle this case!For now restart.
-				//	this.emit("http400");
-				//};
+				if (res.statusCode === 400) {
+					console.log('[HTTP_error_code]: ' + res.statusCode + ' server not responding!');
+					//Add logic to handle this case!For now restart.
+					this.emit("http400");
+				};
 
-				//if (res.statusCode === 502) {
-				//	console.log('[HTTP_error_code]: ' + res.statusCode + ' server not responding!');
-				//	//Add logic to handle this case! For now restart.
-				//	this.emit("http502");
-				//};
+				if (res.statusCode === 502) {
+					console.log('[HTTP_error_code]: ' + res.statusCode + ' server not responding!');
+					//Add logic to handle this case! For now restart.
+					this.emit("http502");
+				};
 
 				res.on('data', function(d) {
     				//process.stdout.write(d);
-    				var httpsRequest = JSON.parse(d);
-    				//console.log("\n" + date() + " [Debug] worldId: " + httpsRequest.world);
-    				//console.log(date() + " [Debug] clientId: " + response.invokerid);
-    				//console.log(date() + " [Debug] clientUid: " + response.invokeruid)
-    				//check for world id 2003 (Gandara)
-    				if (httpsRequest.world === 2003) {
-						var databaseConnection = new sqlite.Database('ts3bot.sqlitedb');
-						//console.log('[db_statement] conect to database: ' + util.inspect(databaseConnection));
+    				if (res.statusCode === 200) {
+	    				var httpsRequest = JSON.parse(d);
+	    				//console.log("\n" + date() + " [Debug] worldId: " + httpsRequest.world);
+	    				//console.log(date() + " [Debug] clientId: " + response.invokerid);
+	    				//console.log(date() + " [Debug] clientUid: " + response.invokeruid)
+	    				//check for world id 2003 (Gandara)
+	    				if (httpsRequest.world === 2003) {
+							var databaseConnection = new sqlite.Database('ts3bot.sqlitedb');
+							//console.log('[db_statement] conect to database: ' + util.inspect(databaseConnection));
 
 
-						databaseConnection.serialize(function() {
+							databaseConnection.serialize(function() {
 
-							var statement = databaseConnection.prepare("UPDATE clients SET gw2_api_key = ? WHERE client_unique_id = ?");
-							statement.run(token, uid);
-							console.log(util.inspect(statement));
-							statement.finalize();
-							//console.log('[db_statement] RUN: ' + util.inspect(statement));
+								var statement = databaseConnection.prepare("UPDATE clients SET gw2_api_key = ? WHERE client_unique_id = ?");
+								statement.run(token, uid, function(response) {
+									console.log('[==== > RUN]' + util.inspect(this));
+									/*If changes have happen permissions are granted otherwise denied.*/
+									if (this.lastID === undefined) {
+				    					console.log(date() + " [Info] FAIL, member permissions denied for " + nick + "( " + uid + " )");
+				    					console.log('[TEST] ' + userId + uid);
+				    					serverQueryClient.send("sendtextmessage", {targetmode: "1", target: userId, msg: 'This key is already in use thus it can not be associated with your account.'}, function (err, response){
+				    					});
+									} else {
+										//console.log("[Debug] sending " + config.confirmAccessMsg);
+										serverQueryClient.send("sendtextmessage", {targetmode: "1", target: userId, msg: config.confirmAccessMsg}, function (err, response){
+											//console.log("[Debug] clientdbid: " + response.cldbid);
+											serverQueryClient.send("clientgetdbidfromuid", {cluid: uid}, function (err, response){
+												console.log(date() + " [Info] SUCCESS, member permissions granted for " + nick + "( " + uid + " )");
+												serverQueryClient.send("servergroupaddclient", {sgid: config.memberServerGroupId, cldbid: response.cldbid}, function (err, response){
+												});
+											});
+										});
+									};
+								});
+								console.log(util.inspect(statement));
+								statement.finalize();
+								//console.log('[db_statement] RUN: ' + util.inspect(statement));
 
-							statement.on("error", function(response) {
-								console.log('\n' + '[db_errorEvent] : ' + response);
-								//console.log('[Inspection] ' + util.inspect(response));
-								if (response.errno === 19) {
-									console.log('[Db_Error]: This API key is already in our database!!');
-								};
-							});
-							statement.on('trace', function(response) {
-								console.log(response);
-							});
-							statement.on('profile', function(response) {
-								console.log(response);
-							});
-						});
-						databaseConnection.close();
-						//console.log("[Debug] sending " + config.confirmAccessMsg);
-						serverQueryClient.send("sendtextmessage", {targetmode: "1", target: userId, msg: config.confirmAccessMsg}, function (err, response){
-							//console.log("[Debug] clientdbid: " + response.cldbid);
-							serverQueryClient.send("clientgetdbidfromuid", {cluid: uid}, function (err, response){
-								console.log(date() + " [Info] SUCCESS, member permissions granted for " + nick + "( " + uid + " )");
-								serverQueryClient.send("servergroupaddclient", {sgid: config.memberServerGroupId, cldbid: response.cldbid}, function (err, response){
+								statement.on("error", function(response) {
+									console.log('\n' + '[db_errorEvent] : ' + response);
+									//console.log('[Inspection] ' + util.inspect(response));
+									if (response.errno === 19) {
+										console.log('[Db_Error]: This API key is already in our database!!');
+									};
+								});
+								statement.on('trace', function(response) {
+									console.log(response);
+								});
+								statement.on('profile', function(response) {
+									console.log(response);
 								});
 							});
-						});
-						
-    				} else {
-    					console.log(date() + " [Info] FAIL, member permissions denied for " + nick + "( " + uid + " )");
-    					serverQueryClient.send("sendtextmessage", {targetmode: "1", target: userId, msg: config.denyAccessMsg}, function (err, response){
-    					});
-    				};
+							databaseConnection.close();
+	    				};
+	    			};
   				});
 
 			}).on('error', function(e) {
   				console.error(e);
   				console.log('[ERROR] error while http.get. Work out a solution for this case!');
+			}).on('http400', function() {
+				serverQueryClient.send("sendtextmessage", {targetmode: "1", target: userId, msg: 'Servers are not responding, please resend me your key.'}, function (err, response){
+				});
+				console.log('HTTP400 error occured.')
+				setTimeout(function() {
+            		ts3bot();
+        		}, 3000);
+			}).on('http502', function() {
+				serverQueryClient.send("sendtextmessage", {targetmode: "1", target: userId, msg: 'Servers are not responding, please resend me your key.'}, function (err, response){
+				});
+				console.log('http502 error occured.')
+				setTimeout(function() {
+            		ts3bot();
+        		}, 3000);
 			});
-
 		}
 		if (response.msg.length === 73 || response.msg.length === 74 || response.msg.length === 75 || response.msg.length === 76) {
 			console.log(date() + " [Info] key not valid (whitespace)..");
@@ -198,18 +220,6 @@ function date() {
 				
 			};
 		};
-	});
-
-	serverQueryClient.on("http400", function() {
-		console.log("TEST400");
-	});
-
-	serverQueryClient.on("http502", function() {
-		console.log("TEST502");
-	});
-
-	serverQueryClient.on("duplicateEntry", function() {
-		console.log('[Duplicate] Duplicate entry of an API-key!');
 	});
 
     serverQueryClient.on("error", function() {
