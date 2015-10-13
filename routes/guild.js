a88d9ef71d54bddc81171fc761d91ef941e7ac81
sfranke <sfr4nke@gmail.com>
2015-10-13 11:41:59 +0200

    Added route to fetch guild details via API.

---
+++ routes/guild.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var util = require('util');
+var https   = require('https');
+
+router.get('/:id', function (req, res, next) {
+
+    var guildJson = {}
+    , guildId       = req.params.id;
+
+    var options = {
+        hostname: 'api.guildwars2.com',
+        path: '/v1/guild_details.json?guild_id=' + guildId,
+        method: 'GET'
+    };
+
+    https.get(options, function (response) {
+        response.on('data', function (data) {
+            switch(response.statusCode) {
+                case 200:
+                    guildJson = JSON.parse(data);
+                    res.json(guildJson);
+                    break;
+
+                default:
+                    console.log('Error while fetching guilds from API.');
+                    res.json({error:'error'});
+                    break;
+            };
+        });
+    });
+});
+
+module.exports = router;
