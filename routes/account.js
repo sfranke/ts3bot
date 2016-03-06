413cc5b0ff80031cc63155edd00532c43c9ef96e
sfranke <sfr4nke@gmail.com>
2016-03-06 09:49:20 +0100

    Account view now works with new database.

--- routes/account.js
+++ routes/account.js
@@ -1,42 +1,33 @@
-var express = require('express');
-var router  = express.Router();
-var util    = require('util');
-var https   = require('https');
+var express     = require('express'),
+    mongoClient = require('mongodb').MongoClient,
+    router      = express.Router(),
+    util        = require('util'),
+    https       = require('https');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.render('account', {title: 'Ts3Bot', name: undefined});
 });
 
-// getAccountinformation = function (Uid, callback) {
-//     var databaseConnectionGet = new sqlite.Database('./ts3bot/ts3bot.sqlitedb');
-//     databaseConnectionGet.serialize(function() {
-//         var statement = databaseConnectionGet.prepare('SELECT * FROM `clients` WHERE `client_unique_id` = (?)');
-//         console.log(statement);
-//         statement.get(Uid, function(error, response) {
-//             console.log('error: ' + error);
-//             console.log('response: ' + util.inspect(response));
-//             if (error != null) {
-//                 callback(error, null);
-//             } else {
-//                 callback(null, response);
-//             };
-//         });
-//         statement.finalize();
-//     });
-//     databaseConnectionGet.close();
-// };
+var uri = 'mongodb://localhost:27017/ts3bot';
+
+getAccountinformation = function(Uid, callback) {
+    mongoClient.connect(uri, function (err, db) {
+        var collection = db.collection('clients');
+        collection.find({client_unique_id: Uid}).limit(1).next(function (err, doc) {
+            if (err) callback(err, null);
+            callback(null, doc);
+            db.close();
+        });
+    });
+};
 
 router.post('/', function (req, res, next) {
-
     var uid = req.body.accountUid;
-
     getAccountinformation(uid, function (error, response) {
-        if (response != undefined) {
-
+        if (response !== undefined) {
             var time = new Date(response.last_seen * 1000);
             var guilds = JSON.parse(response.gw2_guilds);
-
             res.render('account', {
                                     title: 'Ts3Bot',
                                     name: response.client_nickname,
