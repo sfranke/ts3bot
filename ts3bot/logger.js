6892506a95bf79b72e57b20a47b177f6587b60e2
sfranke <sfr4nke@gmail.com>
2016-07-09 00:48:59 +0200

    Clean-up of logger file.

--- ts3bot/logger.js
+++ ts3bot/logger.js
@@ -1,26 +1,26 @@
-var logger = exports,
-    config = JSON.parse(require('fs').readFileSync('config.json')),
-    fs     = require('fs');
+var logger = exports
+var config = JSON.parse(require('fs').readFileSync('config.json'))
+var fs = require('fs')
 
-logger.debuglevel = config.debuglevel;
-fs.openSync(__dirname + '/log', 'a');
+logger.debuglevel = config.debuglevel
+fs.openSync(`${__dirname}/log`, 'a')
 
-function date() {
-    var date = new Date();
-    return date;
+function date () {
+  var date = new Date()
+  return date
 }
 
-logger.log = function(level, message) {
-    var levels = ['error', 'warning', 'info', 'debug'];
-    if (levels.indexOf(level) <= levels.indexOf(logger.debuglevel)) {
-        if (typeof message !== 'string') {
-            message = JSON.stringify(message);
-        }
-        console.log(date() + ' ' + level + ': ' + message);
-        fs.appendFile(__dirname + '/log', date() + ' ' + level + ': ' + message + '\n', function(err) {
-            if (err) {
-                logger.log('error', 'Failed to write to log file!');
-            }
-        });
+logger.log = function (level, message) {
+  var levels = ['error', 'warning', 'info', 'debug']
+  if (levels.indexOf(level) <= levels.indexOf(logger.debuglevel)) {
+    if (typeof message !== 'string') {
+      message = JSON.stringify(message)
     }
-};
+    console.log(date() + ' ' + level + ': ' + message)
+    fs.appendFile(`${__dirname}/log`, date() + ' ' + level + ': ' + message + '\n', function (err) {
+      if (err) {
+        logger.log('error', 'Failed to write to log file!')
+      }
+    })
+  }
+}
