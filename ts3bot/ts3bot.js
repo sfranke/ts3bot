55c1b78516df358a6b32abb84bc853cdf013174c
sfranke <sfr4nke@gmail.com>
2016-11-08 19:20:19 +0100

    Added a chat command to retrieve general information about a client from the TS3 database.

--- ts3bot/ts3bot.js
+++ ts3bot/ts3bot.js
@@ -452,6 +452,7 @@ var config = JSON.parse(require('fs').readFileSync('config.json'));
       serverQueryClient.send('sendtextmessage', message.chatSend('admin', response))
       logger.log('info', 'Received message from admin: ' + '\n' + '\'' + response.msg + '\'')
       logger.log('debug', 'ResponseOnject on AdminMessage: ' + util.inspect(response))
+      var adminID = response.invokerid
       if (response.msg.length > 1) {
         var AdminMessageArray = response.msg.split(' ')
         logger.log('debug', 'AdminMessageArray after split() ' + AdminMessageArray)
@@ -464,6 +465,35 @@ var config = JSON.parse(require('fs').readFileSync('config.json'));
             } else {
               logger.log('info', 'Sending idle poke.')
               serverQueryClient.send('sendtextmessage', {targetmode: '1', target: clid, msg: config.idleMove})
+            }
+          })
+        }
+        // Chat command to find general information a client and whether or not she belongs to
+        // the commander server group.
+        if (AdminMessageArray[0] === '!userInfo') {
+          logger.log('debug', 'arg: ' + AdminMessageArray[1])
+          var uid = AdminMessageArray[1].toString()
+          serverQueryClient.send('clientgetids', {cluid: uid}, function (error, response) {
+            logger.log('debug', 'ClientDBFind error:\n' + util.inspect(error))
+            logger.log('debug', 'ClientDBFind response:\n' + util.inspect(response))
+            if (response !== undefined) {
+              logger.log('debug', 'ClientDBId: ' + util.inspect(response))
+              serverQueryClient.send('clientinfo', {clid: response.clid}, function (error, response) {
+                logger.log('debug', 'clientinfo error:\n' + util.inspect(error))
+                logger.log('debug', 'clientinfo response:\n' + util.inspect(response))
+                if (response !== undefined) {
+                  var userName = response.client_nickname
+                  var lastSeen = new Date(response.client_lastconnected * 1000)
+                  var commander = 'No'
+                  if (response.client_servergroups.indexOf(config.commanderServerGroupId) !== -1) {
+                    logger.log('debug', 'Found commander: ' + response.client_servergroups.indexOf(config.commanderServerGroupId))
+                    commander = 'Yes'
+                  }
+                  var msg = '\n[B]Username[/B]: ' + userName + '\n[B]Last seen[/B]: ' + lastSeen + '\n[B]Commander[/B]: ' + commander
+                  serverQueryClient.send('sendtextmessage', {targetmode: '1', target: adminID,
+                  msg: msg})
+                }
+              })
             }
           })
         }
