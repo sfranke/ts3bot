0d85ad9cded1e8d1eb2896d89ee102d2757eb0da
sfranke <sfr4nke@gmail.com>
2017-02-01 11:09:04 +0100

    Decoupling of databaseBackup and kill commands.

---
+++ ts3bot/adminChatCommands/databaseBackup.js
@@ -0,0 +1,21 @@
+var databaseBackup = exports
+var logger = require('../logger')
+var exec = require('child_process').exec
+var util = require('util')
+
+databaseBackup.info = function () {
+  databaseBackup.issued = '!databaseBackup'
+  databaseBackup.description = 'Create a backup of your clients collection.'
+  databaseBackup.message = 'Creating a database backup.'
+  return databaseBackup
+}
+
+databaseBackup.command = function (client, serverQueryClient) {
+  serverQueryClient.send('sendtextmessage', {targetmode: '1', target: client.invokerid, msg: databaseBackup.info().message})
+  exec('mongoexport -d ts3bot -c clients -o clients_backup.json', function (error, stdout, stderr) {
+    if (error) logger.log('debug', 'Error while creating database dump. ' + util.inspect(error))
+    serverQueryClient.send('sendtextmessage', {targetmode: '1', target: client.invokerid, msg: 'Done.'})
+    logger.log('debug', 'Creating database dump, stderr: ' + util.inspect(stderr))
+    logger.log('debug', 'Creating database dump, stdout: ' + util.inspect(stdout))
+  })
+}
